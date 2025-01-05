e88177d64f7abe548b9735473d6eb908f27f53a1
nawab-khan <nawabkhan845890@gmail.com>
2025-01-05 23:55:15 +0530

    540.optimized

--- BinarySearch/540.singleElementInASortedArray.cpp
+++ BinarySearch/540.singleElementInASortedArray.cpp
@@ -1,7 +1,38 @@
-#include <iostream>
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        
+        int n = nums.size();
 
-using namespace std;
+        if(n == 1)
+            return nums[0];
 
-int main() {
-    
-}
+        int l = 0, r = n - 1, m;
+
+        while(l < r){
+
+            m = l + (r - l)/2;
+
+            if(nums[m] == nums[m+1]){
+                if((r-m)%2 == 0){
+                    l = m + 2;
+                }
+                else{
+                    r = m - 1;
+                }
+            }
+
+            else {
+                if((r-m)%2 == 0){
+                    r = m;
+                }
+                else{
+                    l = m + 1;
+                }
+            }
+
+        }
+            return nums[r];
+        
+    }
+};
