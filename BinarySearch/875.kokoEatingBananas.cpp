f2e795642d979c5c8ded6ac4db5c99a8c3c42efd
nawab-khan <nawabkhan845890@gmail.com>
2025-01-10 01:12:24 +0530

    bs

---
+++ BinarySearch/875.kokoEatingBananas.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        
+        int n = piles.size();
+        long long l = 1, r = INT_MIN, m, total;
+
+        for(int i=0;i<n;i++)
+            if(r < piles[i])
+                r = piles[i];
+
+        while(l <= r){
+
+            m = l + (r - l)/2;
+            total = 0;
+
+            for(int i=0;i<n;i++){
+
+                total += (piles[i]/m + (piles[i]%m == 0 ? 0 : 1));
+
+            }
+
+            if(total > h)
+                l = m + 1;
+            else
+                r = m - 1;
+            
+        }
+
+        return l;
+    }
+};
