06fd4e5d16e2499eb9068e864e3780eea07f2836
Nawab Khan <nawabkhan845890@gmail.com>
2025-03-12 18:41:46 +0530

    DP begins

---
+++ DP/knapsackProblem(using memoization).cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+    int t[1001][1001];
+    
+    int helper(int W, int n, vector<int> &val, vector<int> &wt){
+        
+        // bc
+        if(n == 0 || W == 0){
+            return 0;
+        }
+        
+        if(t[n][W] != -1)
+            return t[n][W];
+        
+        if(wt[n-1] > W)
+            return t[n][W] = helper(W, n-1, val, wt);
+        
+        return t[n][W] = max(helper(W, n-1, val, wt), val[n-1] + helper(W - wt[n-1], n-1, val, wt));
+    }
+    
+    int knapSack(int W, vector<int> &val, vector<int> &wt) {
+        
+        memset(t, -1, sizeof(t));
+        
+        return helper(W, val.size() , val, wt);
+    }
+};
